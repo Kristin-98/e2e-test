3caecfecf590f83e1f41ec77946a6b60a6771c2d
Kristin-98 <kristi98_s@hotmail.se>
2025-09-01 18:06:37 +0200

    added api key

---
+++ app/api/weather/route.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const city = searchParams.get("city");
+
+  if (!city) {
+    return NextResponse.json({ error: "Ingen stad angiven" }, { status: 400 });
+  }
+
+  try {
+    const apiKey = process.env.OPENWEATHER_KEY;
+    console.log("API key:", apiKey);
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+        city
+      )}&appid=${apiKey}&units=metric&lang=sv`
+    );
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: "Stad hittades inte" },
+        { status: 404 }
+      );
+    }
+
+    const data = await res.json();
+
+    return NextResponse.json({
+      city: data.name,
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+    });
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Kunde inte hämta väder just nu" },
+      { status: 500 }
+    );
+  }
+}
